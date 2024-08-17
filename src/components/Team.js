e6c64e256611155e56db04824e5f3e43930d1ba0
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-08-17 10:00:36 -0500

    added my info to employee card

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,40 +1,45 @@
-import React from 'react';
-import { Row, Col, Container } from 'react-bootstrap';
-import EmployeeCard from './smcomponents/EmployeeCard';
-import classes from './team.module.css';
-
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import EmployeeCard from "./smcomponents/EmployeeCard";
+import classes from "./team.module.css";
+import nico_headshot from "../assets/nico_headshot.jpg";
 
 const Team = props => {
-    return (
-        <div className={classes.container} id="team" >
-            <Container >
-                <h1 className={classes.title}>The Team</h1>
-                <Row >
-                    <Col>
-                        <EmployeeCard
-                            title={'Fausto Lopez'}
-                            subtitle={'Founder - Full Stack & Data Engineering'}
-                            email={'faustolopez110@gmail.com'}
-                            github_link={'https://github.com/datafaust'}
-                            image={'https://media.licdn.com/dms/image/C4D03AQF4a2FhUX9jdg/profile-displayphoto-shrink_400_400/0/1553571330316?e=1727913600&v=beta&t=9Ywl6Djbp4Mk14dtD-AV8xjynEMxV_0dxd3YMxoFE6s'}
-                            summary={'John Doe is a seasoned Full Stack Engineer with over 10 years of experience in web development. He excels in creating scalable, high-performance applications using a wide range of technologies, including React, Node.js, and MongoDB. John is passionate about coding best practices and enjoys mentoring junior developers to help them achieve their potential.'}
-                        />
-                    </Col>
-                    <Col>
-                        <EmployeeCard
-                            title={'Nicholas Lopez'}
-                            subtitle={'Co Founder - Full Stack Software Engineer'}
-                            email={'lopez4163@gmail.com '}
-                            github_link={'https://github.com/Lopez4163'}
-                            image={'https://media.licdn.com/dms/image/v2/D5635AQGnFk1zqfjDzQ/profile-framedphoto-shrink_800_800/profile-framedphoto-shrink_800_800/0/1704773114313?e=1723842000&v=beta&t=tJJcmfG2P0a46lkpi8mykCsj8DI_AzwlhOgpQPavItg'}
-                            summary={'John Doe is a seasoned Full Stack Engineer with over 10 years of experience in web development. He excels in creating scalable, high-performance applications using a wide range of technologies, including React, Node.js, and MongoDB. John is passionate about coding best practices and enjoys mentoring junior developers to help them achieve their potential.'}
-                        />
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+  return (
+    <div className={classes.container} id="team">
+      <Container>
+        <h1 className={classes.title}>The Team</h1>
+        <Row>
+          <Col>
+            <EmployeeCard
+              title={"Fausto Lopez"}
+              subtitle={"Founder - Full Stack & Data Engineering"}
+              email={"faustolopez110@gmail.com"}
+              github_link={"https://github.com/datafaust"}
+              image={
+                "https://media.licdn.com/dms/image/C4D03AQF4a2FhUX9jdg/profile-displayphoto-shrink_400_400/0/1553571330316?e=1727913600&v=beta&t=9Ywl6Djbp4Mk14dtD-AV8xjynEMxV_0dxd3YMxoFE6s"
+              }
+              summary={
+                "John Doe is a seasoned Full Stack Engineer with over 10 years of experience in web development. He excels in creating scalable, high-performance applications using a wide range of technologies, including React, Node.js, and MongoDB. John is passionate about coding best practices and enjoys mentoring junior developers to help them achieve their potential."
+              }
+            />
+          </Col>
+          <Col>
+            <EmployeeCard
+              title={"Nicholas Lopez"}
+              subtitle={"Co Founder - Full Stack Software Engineer"}
+              email={"lopez4163@gmail.com "}
+              github_link={"https://github.com/Lopez4163"}
+              image={nico_headshot}
+              summary={
+                "John Doe is a seasoned Full Stack Engineer with over 10 years of experience in web development. He excels in creating scalable, high-performance applications using a wide range of technologies, including React, Node.js, and MongoDB. John is passionate about coding best practices and enjoys mentoring junior developers to help them achieve their potential."
+              }
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
-
 
 export default Team;
--- src/components/smcomponents/EmployeeCard.js
+++ src/components/smcomponents/EmployeeCard.js
@@ -1,63 +1,123 @@
-import React from 'react';
-import { Card, Image, Row, Col } from 'react-bootstrap';
-import classes from './employeeCard.module.css';
+import React from "react";
+import { Card, Image, Row, Col } from "react-bootstrap";
+import classes from "./employeeCard.module.css";
 
 /**IMAGE LOADING */
-import gmail from '../../assets/gmail.png';
-import github from '../../assets/github.png';
-import cv from '../../assets/curriculum-vitae.png';
-import linkedin from '../../assets/linkedin.png';
-import gitlab from '../../assets/gitlab.png'
-import resume from '../../assets/fausto-lopez-resume.pdf';
+import gmail from "../../assets/gmail.png";
+import github from "../../assets/github.png";
+import cv from "../../assets/curriculum-vitae.png";
+import linkedin from "../../assets/linkedin.png";
+import gitlab from "../../assets/gitlab.png";
+import resume from "../../assets/fausto-lopez-resume.pdf";
+import nico_resume from "../../assets/nico_resume.pdf";
 
+const EmployeeCard = props => {
+  return (
+    <Card className={classes.cards}>
+      <Card.Body>
+        <Card.Title>
+          <div className={classes.title}>{props.title}</div>
+        </Card.Title>
 
-const EmployeeCard = (props) => {
-    return (
-        <Card className={classes.cards}>
-            <Card.Body>
-                <Card.Title>
-                    <div className={classes.title}>
-                    {props.title}
-                    </div>
-                </Card.Title>
-                
-                <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '1rem' }}>
-                    <Image 
-                        src={props.image}
-                        alt={props.alt}
-                        roundedCircle
-                        style={{
-                            width: '150px',
-                            height: '150px',
-                            objectFit: 'cover'
-                        }}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "1rem",
+          }}
+        >
+          <Image
+            src={props.image}
+            alt={props.alt}
+            roundedCircle
+            style={{
+              width: "150px",
+              height: "150px",
+              objectFit: "cover",
+            }}
+          />
+        </div>
+
+        <Card.Title>
+          <div className={classes.subtitle}>{props.subtitle}</div>
+        </Card.Title>
+        <Card.Text className={classes.summary}>{props.summary}</Card.Text>
+
+        {/**PANEL */}
+        <Row>
+          <Col>
+            <div style={{ textAlign: "center" }}>
+              <a>
+                <Image
+                  className={classes.icon}
+                  src={gmail}
+                  height={50}
+                  width={50}
+                  onClick={() =>
+                    window.open(`mailto:${props.email}?subject=Contact`)
+                  }
+                />
+              </a>
+              <a href={props.github_link} target="_blank">
+                <Image
+                  className={classes.icon}
+                  src={github}
+                  height={50}
+                  width={50}
+                />
+              </a>
+              {/* <a href='https://gitlab.com/ulysses' target='_blank'><Image className={classes.icon}  src={gitlab} height={50} width={50} /></a> */}
+              {props.title === "Nicholas Lopez" ? (
+                <div className="">
+                  <a
+                    href="https://www.linkedin.com/in/nicholas-lopez-83b031299/"
+                    target="_blank"
+                  >
+                    <Image
+                      className={classes.icon}
+                      src={linkedin}
+                      height={50}
+                      width={50}
                     />
+                  </a>
+                  <a href={nico_resume} download="nico_resume.pdf">
+                    <Image
+                      className={classes.icon}
+                      src={cv}
+                      height={50}
+                      width={50}
+                    />
+                  </a>
                 </div>
-                
-                <Card.Title>
-                    <div className={classes.subtitle}>
-                        {props.subtitle}
-                    </div>
-                </Card.Title>
-                <Card.Text className={classes.summary}>
-                    {props.summary}
-                </Card.Text>
-
-                {/**PANEL */}
-            <Row>
-                <Col>
-                <div style={{ textAlign: 'center' }}>
-                    <a ><Image className={classes.icon} src={gmail} height={50} width={50} onClick={()=>window.open(`mailto:${props.email}?subject=Contact`)} /></a>
-                    <a href='https://www.linkedin.com/in/lopezfausto' target='_blank'><Image className={classes.icon}  src={linkedin} height={50} width={50} /></a>
-                    <a href={props.github_link} target='_blank'><Image className={classes.icon}  src={github} height={50} width={50} /></a>
-                    {/* <a href='https://gitlab.com/ulysses' target='_blank'><Image className={classes.icon}  src={gitlab} height={50} width={50} /></a> */}
-                    <a href={resume} download="fausto-lopez-resume.pdf"><Image className={classes.icon}  src={cv} height={50} width={50} /></a>
+              ) : (
+                <div className="">
+                  <a
+                    href="https://www.linkedin.com/in/lopezfausto"
+                    target="_blank"
+                  >
+                    <Image
+                      className={classes.icon}
+                      src={linkedin}
+                      height={50}
+                      width={50}
+                    />
+                  </a>
+                  <a href={resume} download="fausto-lopez-resume.pdf">
+                    <Image
+                      className={classes.icon}
+                      src={cv}
+                      height={50}
+                      width={50}
+                    />
+                  </a>
                 </div>
-                </Col>
-            </Row>
-            </Card.Body>
-        </Card>
-    );
-}
+              )}
+            </div>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default EmployeeCard;
