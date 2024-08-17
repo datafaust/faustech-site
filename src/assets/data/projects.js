dbc07ae94a7205b29df5cbd58db995b78a99b44f
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-08-17 10:32:30 -0500

    added two projects

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,74 +1,68 @@
 /**IMGE LOADING */
-import db from '../../assets/database_design.png';
+import db from "../../assets/database_design.png";
 // import web_dev from '../../assets/web_dev.jpeg';
-import web_dev from '../../assets/web_development2.png'
-import rpython from '../../assets/r_python.png';
-import data_analytics from '../../assets/data_analytics.jpeg';
-import consulting from '../../assets/ideas.png';
-import rshiny from '../../assets/rshiny.jpeg';
-
-
-
+import web_dev from "../../assets/web_development2.png";
+import rpython from "../../assets/r_python.png";
+import data_analytics from "../../assets/data_analytics.jpeg";
+import consulting from "../../assets/ideas.png";
+import rshiny from "../../assets/rshiny.jpeg";
 
 export const projects = [
-    {
-        'image': db,
-        'title': 'Data Management',
-        'text': 'Deploy and sustain secure database solutions to help you manage your data.',
-        'tag': 'data',
-        'link': '',
-        'date': '2017-01-01'
-    },
-    {
-        'image': data_analytics,
-        'title': 'Data Analysis',
-        'text': 'Analyze data big and small to extract actionable insights.',
-        'tag': 'data',
-        'link': 'https://play.google.com/store/apps/details?id=com.vcoolstudio.doublePark&hl=en_US',
-        'date': '2018-01-01'
-    },
-    {
-        'image': web_dev,
-        'title': 'Web and Application Development',
-        'text': 'Build custom web applications to meet your needs.',
-        'tag': 'react',
-        'link': '',
-        'date': '2019-04-01'
-    },
-    {
-        'image': rpython,
-        'title': 'Data Science Ecosystems',
-        'text': 'Deploy and sustain R shiny server and workbench for an entire data science suite.',
-        'tag': 'react',
-        'link': '',
-        'date': '2019-01-01'
-    },
-    {
-        'image': rshiny,
-        'title': 'Shiny Server',
-        'text': 'Need a mobile application built for your business? We provide services for mobile application design to deployment.',
-        'tag': 'react',
-        'link': '',
-        'date': '2019-07-01'
-    },
-    {
-        'image': consulting,
-        'title': 'Technology Consulting Services',
-        'text': 'Provide technology reccomendations for your business model.',
-        'tag': 'react',
-        'link': '',
-        'date': '2019-04-01'
-    },
-   
-    
-    
-    // {
-    //     'image': react_native,
-    //     'title': 'Mobile Applications',
-    //     'text': 'Need a mobile application built for your business? We provide services for mobile application design to deployment.',
-    //     'tag': 'react',
-    //     'link': '',
-    //     'date': '2019-07-01'
-    // },
+  {
+    image: db,
+    title: "Data Management",
+    text: "Deploy and sustain secure database solutions to help you manage your data.",
+    tag: "data",
+    link: "",
+    date: "2017-01-01",
+  },
+  {
+    image: data_analytics,
+    title: "Data Analysis",
+    text: "Analyze data big and small to extract actionable insights.",
+    tag: "data",
+    link: "https://play.google.com/store/apps/details?id=com.vcoolstudio.doublePark&hl=en_US",
+    date: "2018-01-01",
+  },
+  {
+    image: web_dev,
+    title: "Web and Application Development",
+    text: "Build custom web applications to meet your needs.",
+    tag: "react",
+    link: "",
+    date: "2019-04-01",
+  },
+  {
+    image: rpython,
+    title: "Data Science Ecosystems",
+    text: "Deploy and sustain R shiny server and workbench for an entire data science suite.",
+    tag: "react",
+    link: "",
+    date: "2019-01-01",
+  },
+  {
+    image: rshiny,
+    title: "Shiny Server",
+    text: "Need a mobile application built for your business? We provide services for mobile application design to deployment.",
+    tag: "react",
+    link: "",
+    date: "2019-07-01",
+  },
+  {
+    image: consulting,
+    title: "Technology Consulting Services",
+    text: "Provide technology reccomendations for your business model.",
+    tag: "react",
+    link: "",
+    date: "2019-04-01",
+  },
 
-]
+  // {
+  //     'image': react_native,
+  //     'title': 'Mobile Applications',
+  //     'text': 'Need a mobile application built for your business? We provide services for mobile application design to deployment.',
+  //     'tag': 'react',
+  //     'link': '',
+  //     'date': '2019-07-01'
+  // },
+];
